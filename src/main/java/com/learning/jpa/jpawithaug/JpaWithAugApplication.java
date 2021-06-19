b89b4da1f57568bdf1d934d09779798342022ba3
August Marsalis <aj@Augusts-MacBook-Pro.local>
2021-06-19 18:15:27 -0500

    new jpa project build

---
+++ src/main/java/com/learning/jpa/jpawithaug/JpaWithAugApplication.java
@@ -0,0 +1,13 @@
+package com.learning.jpa.jpawithaug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWithAugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaWithAugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/jpa/jpawithaug/JpaWithAugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.jpa.jpawithaug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWithAugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
